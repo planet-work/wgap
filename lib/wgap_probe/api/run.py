ad2679aba18104de8c0822e16fccbefb4be746b1
Frédéric VANNIÈRE <f.vanniere@planet-work.com>
2017-02-08 09:34:23 +0100

    Flake8 fixes

--- lib/wgap_probe/api/run.py
+++ lib/wgap_probe/api/run.py
@@ -14,12 +14,17 @@ import json
 UID_CACHE = {}
 INTERVAL = 1
 
+
 def get_username(uid):
-     if uid in UID_CACHE:
-         return UID_CACHE[uid]
-     p = pwd.getpwuid(uid)
-     UID_CACHE[uid] = p.pw_name
-     return p.pw_name
+    if uid in UID_CACHE:
+        return UID_CACHE[uid]
+    p = pwd.getpwuid(uid)
+    UID_CACHE[uid] = p.pw_name
+    return p.pw_name
+
+
+def send_output(data):
+    json.dumps(data)
 
 
 def main(**kwargs):
@@ -28,7 +33,6 @@ def main(**kwargs):
     """
     # Using kwargs to provide a generic interface across all commands.
     logger.debug("executing run command")
-    
     bpf_text = ''
     src_path = os.path.dirname(__file__)
     f = open(src_path + '/bpf_probe.c')
@@ -39,14 +43,16 @@ def main(**kwargs):
     bpf_text = bpf_text.replace('TYPE_FILTER', '!S_ISREG(mode)')
 
     if 'gid_min' in config.filter:
-        bpf_text = bpf_text.replace('GID_FILTER','gid < %i' % config.filter.gid_min )
+        gid_min = config.filter.gid_min
+        bpf_text = bpf_text.replace('GID_FILTER', 'gid < %i' % gid_min)
     else:
-        bpf_text = bpf_text.replace('GID_FILTER','0')
+        bpf_text = bpf_text.replace('GID_FILTER', '0')
 
     if 'uid_min' in config.filter:
-        bpf_text = bpf_text.replace('UID_FILTER','uid < %i' % config.filter.uid_min )
+        uid_min = config.filter.uid_min
+        bpf_text = bpf_text.replace('UID_FILTER', 'uid < %i' % uid_min)
     else:
-        bpf_text = bpf_text.replace('UID_FILTER','0')
+        bpf_text = bpf_text.replace('UID_FILTER', '0')
 
     b = BPF(text=bpf_text)
 
@@ -55,9 +61,7 @@ def main(**kwargs):
     if 'file_write' in config.input:
         b.attach_kprobe(event="__vfs_write", fn_name="trace_write_entry")
 
-
     return 0
-
 
     exiting = 0
     while 1:
@@ -71,19 +75,21 @@ def main(**kwargs):
         for k, v in reversed(sorted(counts.items(),
                                     key=lambda counts: counts[1].rbytes)):
             name = k.name
-            if name in ['access.log','processes.log','error.log','other_vhosts_access.log']:
+            if name in config.filter.exclude_files:
                 continue
             k.user = get_username(k.uid)
             print("%s %s" % (k.user, k.name))
-            if not k.user in ['planet-work']:
-                 continue
-            print("%-6d %-16s %-6d %-6d %-7d %-7d %1s %s" % (k.pid, k.user,
-                v.reads, v.writes, v.rbytes / 1024, v.wbytes / 1024, k.type,
-                name))
+            if k.user not in ['planet-work']:
+                continue
+            print(
+              "%-6d %-16s %-6d %-6d %-7d %-7d %1s %s" % (
+                  k.pid, k.user,
+                  v.reads, v.writes, v.rbytes / 1024, v.wbytes / 1024, k.type,
+                  name)
+            )
 
         if exiting:
             print("Detaching...")
             exit()
-
 
     return 0
